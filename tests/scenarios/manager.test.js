fc9c925d6c136479efff39bf732055c84e01015a
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2023-07-06 11:45:14 +0700

    add : filter customer

--- tests/scenarios/manager.test.js
+++ tests/scenarios/manager.test.js
@@ -39,4 +39,10 @@ describe("Customer Test", () => {
     element.click(loginPage.buttonSubmit);
     assert.shouldContainValue(managerPage.userSelect, "");
   });
+  it.only("Filter Customer", () => {
+    element.click(loginPage.managerButtonLogin);
+    element.click(managerPage.customerButton);
+    element.fillFilled(managerPage.searchCustomer, data.VALID_FILTER_CUSTOMER.firstname);
+    assert.shouldContainText(managerPage.tbodyFirstName, data.VALID_FILTER_CUSTOMER.firstname);
+  });
 });
